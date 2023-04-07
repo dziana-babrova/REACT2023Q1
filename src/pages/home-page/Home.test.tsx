d7a03b101bac0ede81d6d318f42b3babd75999d0
dziana-babrova <dzianababrova@gmail.com>
2023-04-07 22:20:59 +0300

    fix: failing tests

--- src/pages/home-page/Home.test.tsx
+++ src/pages/home-page/Home.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it } from 'vitest';
+import { Mock, describe, it, vi } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/react';
 
 import { HomePage } from 'pages/home-page/Home';
@@ -8,9 +8,90 @@ describe('Home page', () => {
     cleanup();
   });
 
-  it('is rendered', () => {
+  it('is rendered', async () => {
+    global.fetch = vi.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            info: {
+              count: 826,
+              pages: 42,
+              next: 'https://rickandmortyapi.com/api/character/?page=2',
+              prev: null,
+            },
+            results: [
+              {
+                id: 1,
+                name: 'Rick Sanchez',
+                status: 'Alive',
+                species: 'Human',
+                type: '',
+                gender: 'Male',
+                origin: {
+                  name: 'Earth',
+                  url: 'https://rickandmortyapi.com/api/location/1',
+                },
+                location: {
+                  name: 'Earth',
+                  url: 'https://rickandmortyapi.com/api/location/20',
+                },
+                image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+                episode: [
+                  'https://rickandmortyapi.com/api/episode/1',
+                  'https://rickandmortyapi.com/api/episode/2',
+                ],
+                url: 'https://rickandmortyapi.com/api/character/1',
+                created: '2017-11-04T18:48:46.250Z',
+              },
+              {
+                id: 17,
+                name: 'Annie',
+                status: 'Alive',
+                species: 'Human',
+                type: '',
+                gender: 'Female',
+                origin: {
+                  name: 'Earth (C-137)',
+                  url: 'https://rickandmortyapi.com/api/location/1',
+                },
+                location: {
+                  name: 'Anatomy Park',
+                  url: 'https://rickandmortyapi.com/api/location/5',
+                },
+                image: 'https://rickandmortyapi.com/api/character/avatar/17.jpeg',
+                episode: ['https://rickandmortyapi.com/api/episode/3'],
+                url: 'https://rickandmortyapi.com/api/character/17',
+                created: '2017-11-04T22:21:24.481Z',
+              },
+              {
+                id: 18,
+                name: 'Antenna Morty',
+                status: 'Alive',
+                species: 'Human',
+                type: 'Human with antennae',
+                gender: 'Male',
+                origin: {
+                  name: 'unknown',
+                  url: '',
+                },
+                location: {
+                  name: 'Citadel of Ricks',
+                  url: 'https://rickandmortyapi.com/api/location/3',
+                },
+                image: 'https://rickandmortyapi.com/api/character/avatar/18.jpeg',
+                episode: [
+                  'https://rickandmortyapi.com/api/episode/10',
+                  'https://rickandmortyapi.com/api/episode/28',
+                ],
+                url: 'https://rickandmortyapi.com/api/character/18',
+                created: '2017-11-04T22:25:29.008Z',
+              },
+            ],
+          }),
+      })
+    ) as Mock;
     render(<HomePage></HomePage>);
     expect(screen.getByRole('search')).toBeInTheDocument();
-    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(await screen.findByRole('list')).toBeInTheDocument();
   });
 });

26559a1e666b5f4f5317adbdb11ae299470e4684
dziana-babrova <dzianababrova@gmail.com>
2023-04-08 22:11:31 +0300

    feat: add loader to modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { CardProps } from 'components/cards/Card';
 import './modal.scss';
 import { getCharacter } from 'services/ApiService';
+import { Loader } from 'components/loader/Loader';
 
 type ModalProps = {
   id: number;
@@ -9,17 +10,19 @@ type ModalProps = {
 
 export const Modal = ({ id }: ModalProps) => {
   const [card, setCard] = useState<CardProps | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getData = async () => {
       setCard(await getCharacter(id));
     };
     getData();
+    setIsLoading(false);
   }, [id]);
 
   return (
     <div className="overlay">
-      <div className="popup">{card?.name}</div>
+      <div className="popup">{isLoading ? <Loader /> : <div>{card?.name}</div>}</div>
     </div>
   );
 };

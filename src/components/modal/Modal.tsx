66a8adb664f2b534ba4687be7bab19553ce0f8ad
dziana-babrova <dzianababrova@gmail.com>
2023-04-08 22:05:28 +0300

    feat: add modal content fetching

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,13 +1,25 @@
+import { useState, useEffect } from 'react';
+import { CardProps } from 'components/cards/Card';
 import './modal.scss';
+import { getCharacter } from 'services/ApiService';
 
 type ModalProps = {
   id: number;
 };
 
 export const Modal = ({ id }: ModalProps) => {
+  const [card, setCard] = useState<CardProps | undefined>(undefined);
+
+  useEffect(() => {
+    const getData = async () => {
+      setCard(await getCharacter(id));
+    };
+    getData();
+  }, [id]);
+
   return (
     <div className="overlay">
-      <div className="popup">{id}</div>
+      <div className="popup">{card?.name}</div>
     </div>
   );
 };
--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -10,3 +10,13 @@ export const getCharacters = async (searchValue: string): Promise<CardProps[]> =
     return [];
   }
 };
+
+export const getCharacter = async (id: number): Promise<CardProps | undefined> => {
+  try {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+    const data = await response.json();
+    return data;
+  } catch {
+    console.log('Data is not fetched');
+  }
+};

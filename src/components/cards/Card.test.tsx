2707e60c3c4655a4c9889890c1c5ba5d88f3b56c
dziana-babrova <dzianababrova@gmail.com>
2023-04-09 12:01:44 +0300

    feat: remove fields from home page cards

--- src/components/cards/Card.test.tsx
+++ src/components/cards/Card.test.tsx
@@ -1,7 +1,7 @@
 import { describe, it } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/react';
 
-import { Card, selectColor } from 'components/cards/Card';
+import { Card } from 'components/cards/Card';
 import data from 'data/data.json';
 
 describe('Card', () => {
@@ -14,27 +14,25 @@ describe('Card', () => {
     render(<Card {...item}></Card>);
     expect(screen.getByAltText(item.name)).toBeInTheDocument();
     expect(screen.getByText(item.name)).toBeInTheDocument();
-    expect(screen.getByText(item.gender)).toBeInTheDocument();
-    expect(screen.getByText(item.location.name)).toBeInTheDocument();
   });
 });
 
-describe('selectColor function', () => {
-  afterEach(() => {
-    cleanup();
-  });
+// describe('selectColor function', () => {
+//   afterEach(() => {
+//     cleanup();
+//   });
 
-  it('returns correct class for alive value', () => {
-    const value = selectColor('alive');
-    expect(value).toBe('card-status_alive');
-  });
+//   it('returns correct class for alive value', () => {
+//     const value = selectColor('alive');
+//     expect(value).toBe('card-status_alive');
+//   });
 
-  it('returns correct class for dead value', () => {
-    const value = selectColor('dead');
-    expect(value).toBe('card-status_dead');
-  });
-  it('returns correct class for any other value', () => {
-    const value = selectColor('unknown');
-    expect(value).toBe('card-status_unknown');
-  });
-});
+//   it('returns correct class for dead value', () => {
+//     const value = selectColor('dead');
+//     expect(value).toBe('card-status_dead');
+//   });
+//   it('returns correct class for any other value', () => {
+//     const value = selectColor('unknown');
+//     expect(value).toBe('card-status_unknown');
+//   });
+// });
--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -14,29 +14,26 @@ export type CardProps = {
   image: string;
 };
 
-const Card = ({ image, name, gender, status, location }: CardProps) => {
+const Card = ({ image, name }: CardProps) => {
   return (
     <div className="cards-item card">
       <img className="card-image" src={image} alt={name} />
       <div className="card-title">
         <span className="card-name">{name} </span>
-        <span className="card-gender">{gender}</span>
       </div>
-      <span className={'card-status ' + selectColor(status)}>{status}</span>
-      <p className="card-location card-info">{location.name}</p>
     </div>
   );
 };
 
-function selectColor(status: string) {
-  switch (status.toLowerCase()) {
-    case 'alive':
-      return 'card-status_alive';
-    case 'dead':
-      return 'card-status_dead';
-    default:
-      return 'card-status_unknown';
-  }
-}
+// function selectColor(status: string) {
+//   switch (status.toLowerCase()) {
+//     case 'alive':
+//       return 'card-status_alive';
+//     case 'dead':
+//       return 'card-status_dead';
+//     default:
+//       return 'card-status_unknown';
+//   }
+// }
 
-export { Card, selectColor };
+export { Card };

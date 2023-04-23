5742c98795661fa04a1a9a0f424ad5a710ad82fb
dziana-babrova <dzianababrova@gmail.com>
2023-04-23 20:38:28 +0300

    feat: add footer

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './header/Header';
+import { Footer } from './footer/Footer';
 
 const Base = () => {
   return (
-    <div>
+    <>
       <Header />
       <Outlet />
-    </div>
+      <Footer />
+    </>
   );
 };
 

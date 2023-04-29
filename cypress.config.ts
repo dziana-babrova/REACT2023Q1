b12eb608debe43d5fadc8883376cf1dcd9eb522a
dziana-babrova <dzianababrova@gmail.com>
2023-04-29 23:27:11 +0300

    feat: add e2e coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'cypress';
+import run from '@cypress/code-coverage/task';
 
 export default defineConfig({
   watchForFileChanges: false,
   video: false,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      run(on, config);
+      return config;
+    },
   },
+  // env: {
+  //   codeCoverage: {
+  //     exclude: 'cypress/**/*.*',
+  //   },
+  // },
 });
